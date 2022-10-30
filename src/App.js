78059ce49fd33050ba537da74e4105e7487c7013
Corey Murphy <coreyimurphy@gmail.com>
2022-10-29 20:33:32 -0400

    feat: home and about boilerplates(not rendering)

--- src/App.js
+++ src/App.js
@@ -3,21 +3,26 @@ import React, { useEffect, useState } from 'react';
 // import jwt_decode from 'jwt-decode';
 // import setAuthToken from './utils/setAuthToken';
 import './App.css';
+import Navbar from './components/navbar/Navbar';
+import { Routes, Route, BrowserRouter as Router} from 'react-router-dom';
 import BarChart from './components/BarChart';
 import DoughnutChart from './components/DoughnutChart';
 import LineChart from './components/LineChart';
 import PieChart from './components/PieChart';
 import Home from './components/Home';
-import Header from './components/header/Header';
-// import { UserData } from './Data'
+import About from './components/About';
 
 
 function App() {
 
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' exact component={Home} />
+        <Route path='/about' component={About} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,14 @@
-import Header from '../components/header/Header';
+import React from 'react';
 
 const Home = () => {
     return(
-        <div>
-            <Header />
+        <div style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '90vh'
+        }}>
+            <h1>Home</h1>
         </div>
     )
 }
