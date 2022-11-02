c59ae5be815556430d025bbfbc51a0137402c205
Amanda Wong <102710833+amanda-w@users.noreply.github.com>
2022-11-01 18:46:01 -0700

    Amanda feature (#7)
    * add: travel and vaccine models
    
    * add: GET, POST, and DELETE routes in travel.js
    
    * add: add put route to travel.js
    
    Co-authored-by: Sooshieroll <102928676+Sooshieroll@users.noreply.github.com>

--- models/travel.js
+++ models/travel.js
@@ -4,8 +4,11 @@ const { Schema } = mongoose;
 const travelSchema = new Schema({
     location: String,
     restriction: Boolean
+
 });
 
 const Travel = mongoose.model('Travel', travelSchema);
 
+
 module.exports = Travel;
+
