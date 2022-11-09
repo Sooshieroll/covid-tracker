c2f4d25ad51823d7f8936634b423ecd3bae6a0e4
Amanda Wong <102710833+amanda-w@users.noreply.github.com>
2022-11-08 19:43:51 -0800

    Fix my issue (#18)
    * add: travel and vaccine models
    
    * add: GET, POST, and DELETE routes in travel.js
    
    * add: add put route to travel.js
    
    * add: routes for vaccine.js
    
    * Lucy/feat seedingdata (#13)
    
    * add: add file
    
    * add: add api and response
    
    * delete front end files
    
    * add: US covid stats
    
    * update: updated routes
    
    * add: api & seeder file
    
    * delete: travel controller
    
    * add seed file & add data
    
    * add: connect API to Vaccines route
    
    * add: users routes
    
    Co-authored-by: Sooshieroll <102928676+Sooshieroll@users.noreply.github.com>

--- controllers/covidStats.js
+++ controllers/covidStats.js
@@ -38,8 +38,6 @@ router.get('/state/:state', (req, res) => {
 })
 
 
-
-
 router.get('/county/:fips', (req, res) => {
     // return axios.get(`https://api.covidactnow.org/v2/county/${req.query.fips}.json?apiKey=${process.env.COVID_API_KEY}`)
     return axios.get(`https://api.covidactnow.org/v2/county/01019.json?apiKey=${process.env.COVID_API_KEY}`)
@@ -75,6 +73,8 @@ router.get('/country/:country/:isocode', (req, res) => {
     });
 
 })
+
+
 
 
 module.exports = router;
